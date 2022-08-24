d58a1ac263779a5539c366ec73a0a461186fa97f
onezer00 <joao.batista@spassu.com-at-702413981770>
2022-08-24 19:49:10 -0300

    Documentação atualizada.

--- setup/settings.py
+++ setup/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import django_heroku
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',
     'escola',
 ]
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- setup/urls.py
+++ setup/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 from escola.views import (AlunosViewSet, CursosViewSet,
                           ListaAlunosMatriculados, ListaMatriculasAluno,
@@ -27,8 +29,20 @@ router.register('cursos', CursosViewSet, basename='cursos')
 router.register('matriculas', MatriculasViewSet, basename='matriculas')
 
 urlpatterns = [
+    path('api_schema/', get_schema_view(
+        title='DJANGO REST API - API Documentation',
+        description = \
+        """
+        Esta API foi desenvolvida para o curso de Orientação a Objetos da Alura.
+        Foram implementadas as seguintes funcionalidades:
+        """
+    ), name='api_schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url':'api_schema'}
+    ), name='swagger-ui'),
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('aluno/<int:pk>/matriculas', ListaMatriculasAluno.as_view()),
     path('curso/<int:pk>/matriculas', ListaAlunosMatriculados.as_view()),
 ]
